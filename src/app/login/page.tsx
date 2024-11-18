fd34d169e04f6936f854cc400feb022eb3cdf4ca
Tylova Pidtrymka <keinaufschub@gmail..com>
2024-11-18 14:31:56 +0200

    feat: added UZ Sans font

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Login() {
+    return <Card className="w-full max-w-md">
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+            <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+            <p>Card Footer</p>
+        </CardFooter>
+    </Card>;
+}
