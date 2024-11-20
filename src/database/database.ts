3f4fabc40c40c2e72392758d484e88f011c6b063
Tylova Pidtrymka <keinaufschub@gmail..com>
2024-11-20 10:46:42 +0200

    feat: added more detailed logging

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+import { Client, Pool } from "pg";
 import { PostgresDialect } from "kysely";
 import { KyselyAuth } from "@auth/kysely-adapter";
 import { up } from "./migrations/001_create_db";
@@ -17,6 +17,23 @@ const dialect = new PostgresDialect({
         max: 10,
     }),
 });
+
+(async () => {
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    });
+
+    try {
+        console.log("Attempting to connect...");
+        await client.connect();
+        console.log("Connected successfully!");
+    } catch (error) {
+        console.error("Connection failed:", error.message);
+    } finally {
+        await client.end();
+    }
+})();
 
 export const db = new KyselyAuth<Database>({
     dialect,
